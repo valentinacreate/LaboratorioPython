68182e1f8687906ca75ef3b179af926051eef4ed
valentinacreate <valentinafurlanis04@gmail.com>
2026-04-19 14:24:27 +0000

    esercizio 3 quasi terminato

--- esercizi/esercizio3.py
+++ esercizi/esercizio3.py
@@ -4,7 +4,18 @@
 #Data:19 aprile 2026
 #
 #Descrizione codice
-#Partendo dal dizionario annidato rubrica:
+#Partendo dal dizionario annidato rubrica
+#   - visualizzate il contenuto del dizionario stampando a schermo delle stringhe formattate che contengano la chiave ed il valor di ognuno degli elementi(Esempio: ‘Paolino Paperino’, ‘giorno’ 9, ‘mese’ ‘giugno’, …)
+#   - A partire dalla rubrica, costruire la lista delle età, ordinata in ordine crescente e visualizzate i nomi in ordine crescente di età
+#   - Invertire l’ordine della lista precedentemente costruita e visualizzatela
+#   - Utilizzare la rubrica e scrivere su schermo per OGNI membro della rubrica, il seguente messaggio:
+#       '''Car[o/a] [Nome],
+#           sei nat[o/a] il [giorno] di [mese] del [anno] e quindi a breve compirai [età] anni.
+#           Ti manderemo gli auguri a [mail]''' dove [o/a] deve essere adattato all’attributo [M/F]
+#Utilizzando args passate in input al vostro programma una chiave [giorno, mese, anno, età, sesso, mail] e visualizzate tutto il contenuto della rubrica (valori) che corrispondono a questa chiave
+#Utilizzando argparse visualizzate la stringa al punto 4 SOLO per il nome fornito come opzione al vostro programma (esempio: python esercizio_3.py –nome ‘Madoka Ayukawa’ –> esegue punto 4 solo per il nome indicato)
+#Utilizzando argparse introducede delle opzioni al vostro programma per eseguire i punti 1, 2, 3, 4, 5 dell’esercizio (esempio: python esercizio_3.py –lista_ordinata –> esegue il punto 2 dell’esercizio)
+
 rubrica = {
     'Paolino Paperino': {'giorno': 9,
                       'mese': 'giugno',
@@ -18,18 +29,54 @@ rubrica = {
                 'età': 46, 
                 'sesso': 'M', 
                 'mail': 'ron_weasley80@hogwards.uk'},
-    'Ramona Flowers': {'giorno': 19, 'mese': 'ottobre', 'anno': 2004, 'età': 22, 'sesso': 'F', 'mail': 'ramona.fls@gmail.com'},
-    'Madoka Ayukawa': {'giorno': 25, 'mese': 'maggio', 'anno': 1969, 'età': 57, 'sesso': 'F', 'mail': 'madoka_sax@asahi_net.jp'}
+    'Ramona Flowers': {'giorno': 19, 
+                'mese': 'ottobre', 
+                'anno': 2004, 
+                'età': 22, 
+                'sesso': 'F', 
+                'mail': 'ramona.fls@gmail.com'},
+    'Madoka Ayukawa': {'giorno': 25, 
+                'mese': 'maggio', 
+                'anno': 1969, 
+                'età': 57, 
+                'sesso': 'F', 
+                'mail': 'madoka_sax@asahi_net.jp'}
 }
 
-#visualizzate il contenuto del dizionario stampando a schermo delle stringhe formattate che contengano la chiave ed il valor di ognuno degli elementi(Esempio: ‘Paolino Paperino’, ‘giorno’ 9, ‘mese’ ‘giugno’, …)
-#A partire dalla rubrica, costruire la lista delle età, ordinata in ordine crescente e visualizzate i nomi in ordine crescente di età
-#Invertire l’ordine della lista precedentemente costruita e visualizzatela
-#Utilizzare la rubrica e scrivere su schermo per OGNI membro della rubrica, il seguente messaggio:
-'''Car[o/a] [Nome],
-sei nat[o/a] il [giorno] di [mese] del [anno] e quindi a breve compirai [età] anni.
-Ti manderemo gli auguri a [mail]'''
-#dove [o/a] deve essere adattato all’attributo [M/F]
-#Utilizzando args passate in input al vostro programma una chiave [giorno, mese, anno, età, sesso, mail] e visualizzate tutto il contenuto della rubrica (valori) che corrispondono a questa chiave
-#Utilizzando argparse visualizzate la stringa al punto 4 SOLO per il nome fornito come opzione al vostro programma (esempio: python esercizio_3.py –nome ‘Madoka Ayukawa’ –> esegue punto 4 solo per il nome indicato)
-#Utilizzando argparse introducede delle opzioni al vostro programma per eseguire i punti 1, 2, 3, 4, 5 dell’esercizio (esempio: python esercizio_3.py –lista_ordinata –> esegue il punto 2 dell’esercizio)
+key_map={
+    'giorno': 'giorno',
+    'mese': 'mese',
+    'anno': 'anno',
+    'età': 'età',
+    'sesso': 'sesso',
+    'mail': 'mail' 
+}
+
+'''Visualizza il contenuto del dizionario stampando a schermo delle stringhe formattate che contengano la chiave ed il valor di ognuno degli elementi(Esempio: ‘Paolino Paperino’, ‘giorno’ 9, ‘mese’ ‘giugno’, …)'''
+print('Contenuto della rubrica:')
+for nome, info in rubrica.items():
+    print(f'{nome}: {info} '+ '\n')
+
+
+'''A partire dalla rubrica, costruisce la lista delle età, ordinata in ordine crescente e visualizza
+   i nomi in ordine crescente di età'''
+def get_eta(nome):
+    return rubrica[nome]['età']
+nomi_ordinati = sorted(rubrica, key=get_eta)
+
+for nome in nomi_ordinati:
+    print(f'{nome}: anni {rubrica[nome]["età"]}')
+
+'''Invertola lista dei nomi in ordine decrescente'''
+for i in range(len(nomi_ordinati)-1,-1,-1):
+    nome=nomi_ordinati[i]
+    print(f'{nome}: anni {rubrica[nomi_ordinati[i]]["età"]}')
+
+'''Per OGNI membro della rubrica, stampa il seguente messaggio:'''
+for nome, info in rubrica.items():
+    if info['sesso']=='M':
+        x='o'
+    else:
+        x='a'
+    print(f'Car{x} {nome}, \nsei nat{x} il {info["giorno"]} di {info["mese"]} del {info["anno"]} e quindi a breve compirai {info["età"]} anni.\nTi manderemo gli auguri a {info["mail"]}')
+
