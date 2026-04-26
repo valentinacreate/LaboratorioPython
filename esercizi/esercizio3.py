b9b329d8587ae04cfa5914752fb7bc56baea22c8
valentinacreate <valentinafurlanis04@gmail.com>
2026-04-26 15:06:22 +0000

    completato es 4, macano commenti in es 3 e 4

--- esercizi/esercizio3.py
+++ esercizi/esercizio3.py
@@ -12,9 +12,9 @@
 #       '''Car[o/a] [Nome],
 #           sei nat[o/a] il [giorno] di [mese] del [anno] e quindi a breve compirai [età] anni.
 #           Ti manderemo gli auguri a [mail]''' dove [o/a] deve essere adattato all’attributo [M/F]
-#Utilizzando args passate in input al vostro programma una chiave [giorno, mese, anno, età, sesso, mail] e visualizzate tutto il contenuto della rubrica (valori) che corrispondono a questa chiave
-#Utilizzando argparse visualizzate la stringa al punto 4 SOLO per il nome fornito come opzione al vostro programma (esempio: python esercizio_3.py –nome ‘Madoka Ayukawa’ –> esegue punto 4 solo per il nome indicato)
-#Utilizzando argparse introducede delle opzioni al vostro programma per eseguire i punti 1, 2, 3, 4, 5 dell’esercizio (esempio: python esercizio_3.py –lista_ordinata –> esegue il punto 2 dell’esercizio)
+#   - Utilizzando args passate in input al vostro programma una chiave [giorno, mese, anno, età, sesso, mail] e visualizzate tutto il contenuto della rubrica (valori) che corrispondono a questa chiave
+#   - Utilizzando argparse visualizzate la stringa al punto 4 SOLO per il nome fornito come opzione al vostro programma (esempio: python esercizio_3.py –nome ‘Madoka Ayukawa’ –> esegue punto 4 solo per il nome indicato)
+#   - Utilizzando argparse introducede delle opzioni al vostro programma per eseguire i punti 1, 2, 3, 4, 5 dell’esercizio (esempio: python esercizio_3.py –lista_ordinata –> esegue il punto 2 dell’esercizio)
 
 import argparse
 
@@ -65,7 +65,6 @@ def ordinamento_eta_crescente(rubrica):
     for eta , nome in lista_eta:
         print(f'{nome}: {eta} anni' + '\n')
 
-
 def ordinamento_eta_decrescente(rubrica):
     '''Inverto la lista dei nomi in ordine decrescente'''
     lista_eta = []
@@ -86,15 +85,22 @@ def stampa_messaggio(rubrica):
             x='a'
         print(f'Car{x} {nome}, \nsei nat{x} il {info["giorno"]} di {info["mese"]} del {info["anno"]} e quindi a breve compirai {info["età"]} anni.\nTi manderemo gli auguri a {info["mail"]}')
 
-'''visualizzare tutto il contenuto della rubrica
-    (valori) che corrispondono a questa chiave'''
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-k', 
-                    '--chiave', 
+parser.add_argument('--chiave', 
                     help='Chiave per filtrare i dati della rubrica')
+parser.add_argument('--stampa_contenuto',
+                    help='Stampa il contenuto della rubrica')
+parser.add_argument('--lista_eta_crescente',
+                    help='Visualizza i nomi in ordine crescente di età')
+parser.add_argument('--lista_eta_decrescente',
+                    help='Visualizza i nomi in ordine decrescente di età')
+parser.add_argument('--nome_esteso', nargs='+',
+                    help='Nome specifico per visualizzare il messaggio personalizzato')
+
 args = parser.parse_args()
+
 def visualizza_contenuto_chiave():
+    '''Visualizza tutto il contenuto della rubrica (valori) che corrispondono a questa chiave'''
     k=args.chiave
     p=True
     while(p==True):
@@ -103,12 +109,47 @@ def visualizza_contenuto_chiave():
                 print(f'Contenuto della rubrica per la chiave "{k}":')
                 for nome, info in rubrica.items():
                     print(info[k])
-                    p=False
+                p=False
             else:
                 print(f'La chiave "{k}" non è valida. Scegli tra "giorno", "mese", "anno", "età", "sesso", "mail".')
-                p=True
-    
+                p=False
+        else:
+            print('Nessuna chiave fornita. Utilizza l\'opzione -k o --chiave per specificare una chiave.')
+            p=False
+
+def visualizza_messaggio_nome_specifico():
+    '''Visualizza il messaggio per un nome specifico nella rubrica'''
+    nome =' '.join(args.nome_esteso)
+    p=True
+    while(p==True):
+        if args.nome_esteso:
+            if nome in rubrica:
+                info = rubrica[nome]
+                if info['sesso']=='M':
+                    x='o'
+                else:
+                    x='a'
+                print(f'Car{x} {nome}, \nsei nat{x} il {info["giorno"]} di {info["mese"]} del {info["anno"]} e quindi a breve compirai {info["età"]} anni.\nTi manderemo gli auguri a {info["mail"]}')
+                p=False
+            else:
+                print(f'Il nome "{nome}" non è valido. Scegli tra i nomi presenti nella rubrica.')
+                p=False
+        else:
+            print('Nessuna chiave fornita. Utilizza l\'opzione -k o --chiave per specificare una chiave.')
+            p=False
+
 def main():
-    visualizza_contenuto_chiave()
+    if args.stampa_contenuto:
+        stampa_contenuto(rubrica)
+    if args.lista_eta_crescente:
+        ordinamento_eta_crescente(rubrica)
+    if args.lista_eta_decrescente:
+        ordinamento_eta_decrescente(rubrica)
+    if args.nome_esteso:
+        visualizza_messaggio_nome_specifico()
+    if args.chiave:
+        visualizza_contenuto_chiave()
+    else:
+        print('ERRORE. Utilizza --help per vedere le opzioni disponibili.')
 
 main()
