2f5341d396b55c8742b096ce675a3acc746ec4bd
valentinacreate <valentinafurlanis04@gmail.com>
2026-04-28 16:50:27 +0000

    es5

--- esercizi/esercizio5.py
+++ esercizi/esercizio5.py
@@ -88,11 +88,13 @@ def dimensione_massima_per_tempo_limite(tempo_limite=15):
     N = 1
     while True:
         
-        while end - start < tempo_limite:
+        while True:
             start = time.perf_counter()
             soluzioni_da_dati(N+1, 1)  # Trova una soluzione per una scacchiera NxN
-            end = time.perf_counter()
+            end = time.perf_counter()       
         if (end - start) > tempo_limite:
+            print(f"Tempo impiegato per dimensione {N+1}: {end - start:.4f} secondi")
             return N - 1  # Restituisce l'ultima dimensione che ha rispettato il limite di tempo
         N += 1
 soluzioni_da_dati(dimensionescacchiera, numerosoluzioni)
+print(f"La dimensione massima della scacchiera per trovare una soluzione in meno di 15 secondi è: {dimensione_massima_per_tempo_limite()}")
