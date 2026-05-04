63a6f7421f8c0790ec7b6ad57d0ebcbe95e63e0e
valentinacreate <valentinafurlanis04@gmail.com>
2026-05-04 07:31:17 +0000

    creazione progetto

---
+++ progetto/MetodoArchimede.py
@@ -0,0 +1,14 @@
+
+# esercizio6.py
+# Creato da Valentina Furlanis IN0501333
+
+# Data: 4 Maggio 2026
+
+#Descrizione del Metodo di Archimede:
+#Il seguente programma esegue l'algoritmo per il calcolo del pigreco
+#il calcolo avviene mediante l'utiizzo di due poligoni con una circonferenza
+#inscritta ad un poligono e circostritta dall'altro
+#aumentando i lati del poligono la vicinanza alla circonferenza aumenta e 
+#l'approssimazione di pigreco migliora
+
+
