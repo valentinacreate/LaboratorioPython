dd20c19b2498b136522f57ebefe38726500b70bd
valentinacreate <valentinafurlanis04@gmail.com>
2026-05-05 09:53:19 +0000

    archimede

--- progetto/MetodoArchimede.py
+++ progetto/MetodoArchimede.py
@@ -10,13 +10,30 @@
 #inscritta ad un poligono e circostritta dall'altro
 #aumentando i lati del poligono la vicinanza alla circonferenza aumenta e 
 #l'approssimazione di pigreco migliora
+import math
 
 class MetodoArchimede:
     def __init__(self):
-        self.poligono_esterno
-        self.poligono_interno
-        self.circonferenza
-        self.pi_greco
+        self.raggio=1.0
         
-        pass
-    pass
+    def calcolo_pi(self, itera):
+        n=6
+        perimetro_circoscritto=(n*2*self.raggio)/math.sqrt(3)
+        perimetro_insritto=n*self.raggio
+
+        for i in range(itera):
+            pi_min = perimetro_circoscritto / (2 * self.raggio)
+            pi_max = perimetro_circoscritto / (2 * self.raggio)
+
+            print(f"\n{i+1}^ interazione")
+            print('Il valore di pi greco è compreso tra:')
+            print(f"{pi_min} data dal poligono circoscritto con {n} lati \n{pi_max} data dal poligono inscritto con {n} lati")
+            
+            perimetro_circoscritto = (2 * perimetro_circoscritto * perimetro_insritto) / (perimetro_circoscritto + perimetro_insritto)
+            perimetro_insritto = math.sqrt(perimetro_circoscritto * perimetro_insritto)
+            n *= 2
+
+arch = MetodoArchimede()
+print('quante volte vuoi iterare l\'algoritmo?')
+numero_iterazione=int(input())
+arch.calcolo_pi(numero_iterazione)
