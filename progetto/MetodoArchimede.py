1cbfea1bb09f17a2abc6dbba12bfeb807febfa48
valentinacreate <valentinafurlanis04@gmail.com>
2026-05-11 07:21:19 +0000

    progetto

--- progetto/MetodoArchimede.py
+++ progetto/MetodoArchimede.py
@@ -10,29 +10,61 @@
 #inscritta ad un poligono e circostritta dall'altro
 #aumentando i lati del poligono la vicinanza alla circonferenza aumenta e 
 #l'approssimazione di pigreco migliora
+#https://trinket.io/library/trinkets/create?lang=pygame per la simulazione
+
 import math
 import turtle
 
 class MetodoArchimede:
     def __init__(self):
-        self.raggio = 1.0
+        self.raggio = 200.0
         self.schermo = turtle.Screen()
-        self.schermo.setup(800, 800)
+        self.schermo.setup(900, 600)
         self.penna = turtle.Turtle()
         self.penna.hideturtle()
-        self.penna.speed("fastest")
+        self.penna.speed("fast")
     
-    def disegna_iterazione(n, pi_min, pi_max):
+    def disegna_iterazione(self, n, pi_min, pi_max):
+        self.penna.clear()
         self.disegna_cerchio()
         self.disegna_poligono_inscritto(n, pi_max)
         self.disegna_poligono_circoscritto(n, pi_min)
-        pass
 
+    def disegna_cerchio(self):
+        self.penna.penup()
+        self.penna.goto(0, -self.raggio)
+        self.penna.pendown()
+        self.penna.circle(self.raggio)
+
+    def disegna_poligono_inscritto(self, n, pi_max=None):
+        angolo = 360 / n
+        self.penna.penup()
+        x = self.raggio * math.cos(math.radians(0))
+        y = self.raggio * math.sin(math.radians(0))
+        self.penna.goto(x, y)
+        self.penna.pendown()
+        for i in range(1, n + 1):
+            x = self.raggio * math.cos(math.radians(i * angolo))
+            y = self.raggio * math.sin(math.radians(i * angolo))
+            self.penna.goto(x, y)
+
+    def disegna_poligono_circoscritto(self, n, pi_min=None):
+        raggio_circoscritto = self.raggio / math.cos(math.pi / n)
+        angolo = 360 / n
+        self.penna.penup()
+        x = raggio_circoscritto * math.cos(math.radians(0))
+        y = raggio_circoscritto * math.sin(math.radians(0))
+        self.penna.goto(x, y)
+        self.penna.pendown()
+        for i in range(1, n + 1):
+            x = raggio_circoscritto * math.cos(math.radians(i * angolo))
+            y = raggio_circoscritto * math.sin(math.radians(i * angolo))
+            self.penna.goto(x, y)
 
     def calcolo_pi(self, itera, grafica = False):
         n = 6
         perimetro_circoscritto = (n * 2 * self.raggio) / math.sqrt(3)
-        perimetro_insritto = n * self.raggio
+        perimetro_inscritto = n * self.raggio
 
         for i in range(itera):
             pi_min = perimetro_circoscritto / (2 * self.raggio)
@@ -42,9 +74,10 @@ class MetodoArchimede:
             print(f"{pi_min} data dal poligono circoscritto con {n} lati \n{pi_max} data dal poligono inscritto con {n} lati")
             
             if grafica:
-                self.disegna_iterazione(n, pi_min, pi_max )
-            perimetro_circoscritto = (2 * perimetro_circoscritto * perimetro_insritto) / (perimetro_circoscritto + perimetro_insritto)
-            perimetro_insritto = math.sqrt(perimetro_circoscritto * perimetro_insritto)
+                self.disegna_iterazione(n, pi_min, pi_max)
+            
+            perimetro_circoscritto = (2 * perimetro_circoscritto * perimetro_inscritto) / (perimetro_circoscritto + perimetro_inscritto)
+            perimetro_inscritto = math.sqrt(perimetro_circoscritto * perimetro_inscritto)
             n *= 2
         if grafica:
             turtle.done()
@@ -52,4 +85,4 @@ class MetodoArchimede:
 arch = MetodoArchimede()
 print('quante volte vuoi iterare l\'algoritmo?')
 numero_iterazione=int(input())
-arch.calcolo_pi(numero_iterazione)
+arch.calcolo_pi(numero_iterazione, grafica=True)
