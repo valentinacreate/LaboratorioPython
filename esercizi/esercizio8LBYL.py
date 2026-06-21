9c3be7d92a10b810b42ef3b9f7aae7316ed1b2d9
valentinacreate <valentinafurlanis04@gmail.com>
2026-06-21 19:17:24 +0000

    es8LBYL grafica

--- esercizi/esercizio8LBYL.py
+++ esercizi/esercizio8LBYL.py
@@ -6,12 +6,12 @@
 
 # Descrizione dell’esercizio8.py:
 #Scrivete un programma per “il gioco dell’impiccato” in cui:
-#-leggete una lista di parole da un file JSON
-#-scegliete una parola a caso con cui giocare dalla lista letta, tramite random
-#-chiedete continuamente all’utente di inserire una lettera o di indovinare la parola, fino al termine del gioco in cui si esauriscono i tentativi o si indovina la parola
-#   La rappresentazione grafica del gioco è libera, così come il numero dei “tentativi” disponibili.
+#- leggete una lista di parole da un file JSON
+#- scegliete una parola a caso con cui giocare dalla lista letta, tramite random
+#- chiedete continuamente all’utente di inserire una lettera o di indovinare la parola, fino al termine del gioco in cui si esauriscono i tentativi o si indovina la parola
+#- La rappresentazione grafica del gioco è libera, così come il numero dei “tentativi” disponibili.
 
-#   1- Scrivete il programma con un approccio totalmente LBYL
+#- 1- Scrivete il programma con un approccio totalmente LBYL
 #   2- RI-scrivete il programma con un approccio totalmente EAFP
 
 import random                                                              
