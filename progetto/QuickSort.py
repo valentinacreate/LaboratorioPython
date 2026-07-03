fd36516de4499c597f0b97d7ed53db8256ac26d5
valentinacreate <valentinafurlanis04@gmail.com>
2026-07-03 17:08:50 +0000

    commenti

--- progetto/QuickSort.py
+++ progetto/QuickSort.py
@@ -17,20 +17,20 @@ class QuickSort:
     
     def partition(array, p, r):
         '''partiziona l'array in base al pivot scelto in modo casuale e restituisce l'indice delpivot'''
-        indice_pivot = random.randint(p, r)
-        pivot=array[indice_pivot]
-        array[indice_pivot], array[r] = array[r], array[indice_pivot]
-        ultimo_valore_minore = p - 1
-        for j in range(p, r):
-            if array[j] < pivot:
+        indice_pivot = random.randint(p, r)                                                         #scelta casuale dell'indice del pivot tra p e r
+        pivot=array[indice_pivot]                                                                   #assegnazione del valore del pivot scelto alla variabile pivot
+        array[indice_pivot], array[r] = array[r], array[indice_pivot]                               #scambio del pivot con l'ultimo elemento dell'array  
+        ultimo_valore_minore = p - 1                                                                #inizializzazione dell'indice dell'ultimo valore minore del pivot a p-1
+        for j in range(p, r):                                                                       #per ogni elemento dell'array tra p e r, confronto l'elemento i-esimo con il pivot
+            if array[j] < pivot:                                                                    #se è minore del pivot, incrementa l'indice dell'ultimo valore minore e scambia l'elemento i-esimo con l'ultimo elemento minore del pivot
                 ultimo_valore_minore += 1
                 array[ultimo_valore_minore], array[j] = array[j], array[ultimo_valore_minore]
-        array[ultimo_valore_minore + 1], array[r] = array[r], array[ultimo_valore_minore +1]
-        return ultimo_valore_minore + 1
+        array[ultimo_valore_minore + 1], array[r] = array[r], array[ultimo_valore_minore +1]        #scambia il pivot con l'elemento successivo all'ultimo valore minore
+        return ultimo_valore_minore + 1                                                             #restituisce l'indice del pivot
     
     def quick_sort(array, p, r):
         '''ordina l'array in modo ricorsivo'''
-        if p < r:
+        if p < r:                                                                                   #finché p è minore di r, eseguo la partizione e poi richiamo la funzione quick_sort() sui due sottoarray generati dalla partizione
             q = QuickSort.partition(array, p, r)
             QuickSort.quick_sort(array, p, q-1)
             QuickSort.quick_sort(array, q + 1, r)
