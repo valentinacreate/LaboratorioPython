6850823e11c525e0e528cddab19582fab34ea497
valentinacreate <valentinafurlanis04@gmail.com>
2026-07-03 16:49:34 +0000

    commenti

--- progetto/MetodoArchimede.py
+++ progetto/MetodoArchimede.py
@@ -20,51 +20,50 @@ class MetodoArchimede:
         self.raggio = 1.0
 
     def calcolo_pi(self, itera):
-        '''calcola il valore approssimato di pi-greco partende da poligoni dinlato 6 e 
-        raddoppiandoli a ogni iterazione'''
-        n = 6
-        perimetro_circoscritto = (n * 2 * self.raggio) / math.sqrt(3)
-        perimetro_inscritto = n * self.raggio
+        '''calcola il valore approssimato di pi-greco partende da poligoni dinlato 6 e raddoppiandoli a ogni iterazione'''
+        n = 6                                                                                   #inizializzazione numero dei lati dei poligoni
+        perimetro_circoscritto = (n * 2 * self.raggio) / math.sqrt(3)                           #calcolo del perimetro del poligono circoscritto 
+        perimetro_inscritto = n * self.raggio                                                   #calcolo del perimetro del poligono inscritto 
 
-        for i in range(itera):
-            pi_min = perimetro_circoscritto / (2 * self.raggio)
-            pi_max = perimetro_inscritto / (2 * self.raggio)
+        for i in range(itera):                                                                  #ciclo for che calcola pi-greco per il numero di iterazioni richieste dall'utente
+            pi_min = perimetro_circoscritto / (2 * self.raggio)                                 #cacolo del valore minimo di pi-greco data dal poligono circoscritto
+            pi_max = perimetro_inscritto / (2 * self.raggio)                                    #cacolo del valore minimo di pi-greco data dal poligono inscritto
+            
+            #stampo i risultati dell'terazione corrente
             print(f"\n{i+1}^ interazione")
             print('Il valore di pi greco è compreso tra:')
             print(f"{pi_min} data dal poligono circoscritto con {n} lati \n{pi_max} data dal poligono inscritto con {n} lati")
             
-            perimetro_circoscritto = (2 * perimetro_circoscritto * perimetro_inscritto) / (perimetro_circoscritto + perimetro_inscritto)
-            perimetro_inscritto = math.sqrt(perimetro_circoscritto * perimetro_inscritto)
+            perimetro_circoscritto = (2 * perimetro_circoscritto * perimetro_inscritto) / (perimetro_circoscritto + perimetro_inscritto)        #calcolo del nuovo perimetro del poligono circoscritto
+            perimetro_inscritto = math.sqrt(perimetro_circoscritto * perimetro_inscritto)                                                       #calcolo del nuovo perimetro del poligono inscritto
             n *= 2
     
     def main():
-        risposte_possibili = ["SI", "NO"]
-        arch = MetodoArchimede()
+        risposte_possibili = ["SI", "NO"]                                                   #lista delle risposte possibile
+        arch = MetodoArchimede()                                                            #creazione di un oggetto della classe MetodoArchimede
         while True:
-            try:
+            try:                                                                            #esecuzione del blocco di codice per chiedere all'utente se vuole osservare l'algoritmo graficamnete che potrebbe generare un'eccezione 
                 print("\nVuoi vedere l\'algoritmo graficamente? [SI/NO]")
                 risposta = input().upper()
                 risposte_possibili.index(risposta)
-            except ValueError:
+            except ValueError:                                                              #gestione dell'eccezione se l'input non è una delle risposte possibili: stampa un messaggio di errore e chiede all'utente di riprovare
                 print("Errore nell'input. Riprova.")
                 continue
-            except KeyboardInterrupt:
+            except KeyboardInterrupt:                                                       #gestione dell'eccezione se l'utente interrompe il programma da tastiera: stampa un messaggio d'errore e chiude il programma
                 print("\nErrore, chiusura gioco")
                 break
-            if risposta == "SI":
-                '''apre una pagina trinked dov'è presente una versione del codice con un\'implementazione grafica 
-                delle iterazioni e mostra visivamente l\'avvicinarsi dei poligoni alla circonferenza'''
+            if risposta == "SI":                                                            #se la risposta è sì apre una pagina trinked dov'è presente una versione del codice con un\'implementazione grafica delle iterazioni e mostra visivamente l\'avvicinarsi dei poligoni alla circonferenza
                 url = "https://trinket.io/pygame/0e0d0a5d314d"
                 webbrowser.open(url)
-            else:
-                try:
+            else:                                                                           #altrimenti chiede all'utente quante iterazioni vuole eseguire e invoca il metodo calcolo_pi()
+                try:                                                                        #esecuzione del blocco di codice per inserire il numero di iterazioni che potrebbe generare un'eccezione
                     print('\nQuante volte vuoi iterare l\'algoritmo?\n Inserire un valore intero positivo')
                     numero_iterazione=int(input())
                     arch.calcolo_pi(numero_iterazione)
-                except ValueError:
+                except ValueError:                                                          #gestione dell'eccezione se l'input non è un numero intero positivo: stampa un messaggio di errore e chiede all'utente di riprovare
                     print("Errore nell'input. Riprova.")
                     continue
-                except KeyboardInterrupt:
+                except KeyboardInterrupt:                                                   #gestione dell'eccezione se l'utente interrompe il programma da tastiera: stampa un messaggio d'errore e chiude il programma
                     print("\nErrore, chiusura gioco")
                     break
             break
